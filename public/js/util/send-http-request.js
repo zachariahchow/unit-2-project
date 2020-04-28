d92d654b955fe5e9fbd3efb5f46297025b65b067
Zachariah Chow <zachariahchow@gmail.com>
2020-04-28 22:46:04 +0800

    added AJAX utility, gear-all views with inputs and select to add gear

---
+++ public/js/util/send-http-request.js
@@ -0,0 +1,21 @@
+// AJAX //
+const sendHttpRequest = async (httpMethod, url, data, httpHeaders) => {
+
+    const response = await fetch(url, {
+        method: httpMethod,
+        body: JSON.stringify(data),
+        headers: httpHeaders
+    })
+
+    if (response.status >= 200 && response.status < 300) {
+        return response.json();
+
+    } else {
+
+        return response.json().then(errData => {
+            console.log(errData);
+            throw new Error('Server-side Error.');
+        })
+    }
+}
+//
