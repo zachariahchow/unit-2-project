38807339c8ba4521657804ecf16775d957e4c4af
Zachariah Chow <zachariahchow@gmail.com>
2020-04-28 15:12:09 +0800

    consolidated tables, added seed data for gear (user_id: 1)

--- sql/all-tables.sql
+++ sql/all-tables.sql
@@ -7,3 +7,45 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 --
+
+CREATE TABLE IF NOT EXISTS gear (
+	id serial PRIMARY KEY,
+	user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+	name VARCHAR NOT NULL,
+	type VARCHAR NOT NULL,
+	img_link VARCHAR,
+	notes VARCHAR
+);
+
+--
+
+CREATE TABLE IF NOT EXISTS pedalboards (
+	id serial PRIMARY KEY,
+	name VARCHAR NOT NULL,
+	user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+
+--
+
+CREATE TABLE IF NOT EXISTS lists (
+	id serial PRIMARY KEY,
+	name VARCHAR NOT NULL,
+	user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+
+--
+
+CREATE TABLE IF NOT EXISTS pedalboards_gear (
+	id serial PRIMARY KEY,
+	pedalboard_id INTEGER REFERENCES pedalboards(id) ON DELETE CASCADE,
+	gear_id INTEGER REFERENCES gear(id) ON DELETE CASCADE,
+	gear_order INTEGER
+);
+
+--
+
+CREATE TABLE IF NOT EXISTS lists_pedalboards (
+	id serial PRIMARY KEY,
+	list_id INTEGER REFERENCES lists(id) ON DELETE CASCADE,
+	pedalboard_id INTEGER REFERENCES pedalboards(id) ON DELETE CASCADE
+);
---
+++ sql/seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO gear(user_id, name, type, img_link) VALUES('1', 'Fender Modern Player Telecaster Thinline Deluxe', 'guitar', 'https://media.karousell.com/media/photos/products/2020/04/08/fender_modern_player_telecaster_thinline_deluxe_3color_sunburst_1586328044_2c5b6e94_progressive.jpg');
+
+INSERT INTO gear(user_id, name, type, img_link) VALUES('1', 'Line 6 Helix-Lt', 'pedal', 'https://media.sweetwater.com/store/enhanced/items/HelixLT/d586b6-HelixLT.jpg');
+
+INSERT INTO gear(user_id, name, type, img_link) VALUES('1', 'Vox AC-15 Half-Stack (2x12" Cab)', 'amp', 'https://media.sweetwater.com/api/i/q-82__ha-5a8f650d48d06612__hmac-bdab6da6e16ac9fa212e77e77c1c8a3200ca2637/images/items/750/AC15Stack-large.jpg');
+
+INSERT INTO gear(user_id, name, type, img_link) VALUES('1', 'Planet Waves American Stage Guitar Cable \(single right-angled tip\)', 'accessory', 'https://images-na.ssl-images-amazon.com/images/I/41aJnQosiRL.jpg');
+
+INSERT INTO gear(user_id, name, type, img_link) VALUES('1', 'Planet Waves American Stage Guitar Cable \(straight tips\)', 'accessory', 'https://images-na.ssl-images-amazon.com/images/I/41aJnQosiRL.jpg');
+
+--
