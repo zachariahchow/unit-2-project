78dd4cbe9a7ea7ac85bc3a827dde224ab9761ccd
Zachariah Chow <zachariahchow@gmail.com>
2020-04-28 23:53:16 +0800

    modified error handling for filter ajax

--- public/js/util/send-http-request.js
+++ public/js/util/send-http-request.js
@@ -1,21 +1,26 @@
 // AJAX //
 const sendHttpRequest = async (httpMethod, url, data, httpHeaders) => {
 
-    const response = await fetch(url, {
-        method: httpMethod,
-        body: JSON.stringify(data),
-        headers: httpHeaders
-    })
+    try {
 
-    if (response.status >= 200 && response.status < 300) {
-        return response.json();
-
-    } else {
-
-        return response.json().then(errData => {
-            console.log(errData);
-            throw new Error('Server-side Error.');
+        const response = await fetch(url, {
+            method: httpMethod,
+            body: JSON.stringify(data),
+            headers: httpHeaders
         })
+
+        if (response.status >= 200 && response.status < 300) {
+            return response.json();
+
+        } else {
+
+            return response.json().then(errData => {
+                console.log(errData);
+                throw new Error('Server-side Error.');
+            })
+        }
+    } catch (err) {
+        console.log(err);
     }
 }
 //
