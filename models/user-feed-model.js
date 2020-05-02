6058d15b6cb7e9bfd9be6cf8a60e6051d5258878
Zachariah Chow <zachariahchow@gmail.com>
2020-05-03 01:11:10 +0800

    fixed puppeteer args option for heroku deploy

--- models/user-feed-model.js
+++ models/user-feed-model.js
@@ -23,7 +23,7 @@ module.exports = {
     // }
 
     getVideosFromYoutube: async function(query) {
-        const browser = await puppeteer.launch({ headless: true, args: ['--nosandbox', '--disable-setuid-sandbox'] });
+        const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
         const page = await browser.newPage();
 
         // const navPromise = page.waitForNavigation();
