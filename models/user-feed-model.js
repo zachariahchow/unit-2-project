d6019a37549161ed796e916488125c102a12b7e9
Zachariah Chow <zachariahchow@gmail.com>
2020-05-04 20:12:59 +0800

    added error handling to user-feed

--- models/user-feed-model.js
+++ models/user-feed-model.js
@@ -23,44 +23,49 @@ module.exports = {
     // }
 
     getVideosFromYoutube: async function(query) {
-        const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
-        const page = await browser.newPage();
+        try {
+            const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+            const page = await browser.newPage();
 
-        // const navPromise = page.waitForNavigation();
+            // const navPromise = page.waitForNavigation();
 
-        await page.goto(`https://www.youtube.com/`);
-        await page.waitForSelector('input#search');
-        await page.type('input#search', query);
+            await page.goto(`https://www.youtube.com/`);
+            await page.waitForSelector('input#search');
+            await page.type('input#search', query);
 
-        // await Promise.all([page.click('#search-icon-legacy'), page.waitForNavigation({ timeout: 0 })])
-        await page.click('#search-icon-legacy');
+            // await Promise.all([page.click('#search-icon-legacy'), page.waitForNavigation({ timeout: 0 })])
+            await page.click('#search-icon-legacy');
 
-        const timeout = (ms) => {
-            return new Promise(resolve => setTimeout(resolve, ms));
+            const timeout = (ms) => {
+                return new Promise(resolve => setTimeout(resolve, ms));
+            }
+
+            await timeout(700);
+            // await page.waitForNavigation({ timeout: 0, waitUntil: 'load' });
+
+            const getVideoLinks = await page.evaluate(() => {
+                let videoLinks = [];
+
+                const videoTags = document.querySelectorAll('a#video-title');
+
+                for (let i = 0; i < 5; i++) {
+                    console.log(videoTags[i].href.split("=")[1]);
+                    videoLinks.push(videoTags[i].href.split("=")[1]);
+                }
+                // videoTags.forEach(tag => {
+                //     console.log(tag.href);
+                //     videoLinks.push(tag.href.split("=")[1]);
+                // });
+
+                return videoLinks;
+            })
+
+            console.log(getVideoLinks);
+            await browser.close();
+            return getVideoLinks;
         }
 
-        await timeout(700);
-        // await page.waitForNavigation({ timeout: 0, waitUntil: 'load' });
-
-        const getVideoLinks = await page.evaluate(() => {
-            let videoLinks = [];
-
-            const videoTags = document.querySelectorAll('a#video-title');
-
-            for (let i = 0; i < 5; i++) {
-                console.log(videoTags[i].href.split("=")[1]);
-                videoLinks.push(videoTags[i].href.split("=")[1]);
-            }
-            // videoTags.forEach(tag => {
-            //     console.log(tag.href);
-            //     videoLinks.push(tag.href.split("=")[1]);
-            // });
-
-            return videoLinks;
-        })
-
-        console.log(getVideoLinks);
-        await browser.close();
-        return getVideoLinks;
+    } catch (err) {
+        console.log(err);
     }
 }
